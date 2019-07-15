135e6038af1c306c63fdf15202e93c2f97c88595
sanketkumar <sanket.kr7922@gmail.com>
2019-07-15 23:03:55 +0530

    Updates

---
+++ precache-manifest.b19b16d8113fdded3dc0f1e69a308744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0725e240cec0b3ecd7794dc8d029e29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a8550ee0174e27abca31",
+    "url": "/monsters-rolodex/static/css/main.c10aebe1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a8550ee0174e27abca31",
+    "url": "/monsters-rolodex/static/js/main.c246e310.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
